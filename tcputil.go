ada671a048a935c5eb149ae89e46cc1df220e3b2
Daisuke Maki <lestrrat+github@gmail.com>
2014-01-29 11:06:19 +0900

    initial commit

---
+++ tcputil.go
@@ -0,0 +1,43 @@
+package tcputil
+
+import (
+  "errors"
+  "fmt"
+  "math/rand"
+  "net"
+  "time"
+)
+
+// Looks for an empty port to listen on local interface.
+func EmptyPort() (int, error) {
+  for p := 50000 + rand.Intn(1000); p < 60000; p++ {
+    l, e := net.Listen("tcp", fmt.Sprintf(":%d", p))
+    if e == nil {
+      // yey!
+      l.Close()
+      return p, nil
+    }
+  }
+
+  return 0, errors.New("Could not find an available port")
+}
+
+// Wait until you can connect to `addr`, up to `dur` amount of time
+func WaitPort(addr string, dur time.Duration) error {
+  timeout := time.Now().Add(dur)
+  for time.Now().Before(timeout) {
+    c, e := net.Dial("tcp", addr)
+    if e == nil {
+      c.Close()
+      return nil
+    }
+  }
+  return errors.New(
+    fmt.Sprintf("Could not connect to '%s'", addr),
+  )
+}
+
+// Wait until you can connect to `port` on localhost, up to `dur` amount of time
+func WaitLocalPort(port int, dur time.Duration) error {
+  return WaitPort(fmt.Sprintf(":%d", port), dur)
+}
