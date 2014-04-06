1e95547dea491f42cfb510e461a1c889eee6b1ae
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-06 21:19:15 +0900

    some linting

--- tcputil.go
+++ tcputil.go
@@ -8,7 +8,7 @@ import (
   "time"
 )
 
-// Looks for an empty port to listen on local interface.
+// EmptyPort looks for an empty port to listen on local interface.
 func EmptyPort() (int, error) {
   for p := 50000 + rand.Intn(1000); p < 60000; p++ {
     l, e := net.Listen("tcp", fmt.Sprintf(":%d", p))
@@ -19,10 +19,10 @@ func EmptyPort() (int, error) {
     }
   }
 
-  return 0, errors.New("Could not find an available port")
+  return 0, errors.New("error: Could not find an available port")
 }
 
-// Wait until you can connect to `addr`, up to `dur` amount of time
+// WaitPort waits until you can connect to `addr`, up to `dur` amount of time
 func WaitPort(addr string, dur time.Duration) error {
   timeout := time.Now().Add(dur)
   for time.Now().Before(timeout) {
@@ -33,12 +33,10 @@ func WaitPort(addr string, dur time.Duration) error {
     }
     time.Sleep(500 * time.Millisecond)
   }
-  return errors.New(
-    fmt.Sprintf("Could not connect to '%s'", addr),
-  )
+  return fmt.Errorf("error: Could not connect to '%s'", addr)
 }
 
-// Wait until you can connect to `port` on localhost, up to `dur` amount of time
+// WaitLocalPort until you can connect to `port` on localhost, up to `dur` amount of time
 func WaitLocalPort(port int, dur time.Duration) error {
   return WaitPort(fmt.Sprintf(":%d", port), dur)
 }
