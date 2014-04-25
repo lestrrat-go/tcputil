3cae7c8432aeb817399f001e0db3f5bb04e8b8a6
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-25 14:59:51 +0900

    I'm a born again go-hard-tabber

--- tcputil.go
+++ tcputil.go
@@ -1,42 +1,42 @@
 package tcputil
 
 import (
-  "errors"
-  "fmt"
-  "math/rand"
-  "net"
-  "time"
+	"errors"
+	"fmt"
+	"math/rand"
+	"net"
+	"time"
 )
 
 // EmptyPort looks for an empty port to listen on local interface.
 func EmptyPort() (int, error) {
-  for p := 50000 + rand.Intn(1000); p < 60000; p++ {
-    l, e := net.Listen("tcp", fmt.Sprintf(":%d", p))
-    if e == nil {
-      // yey!
-      l.Close()
-      return p, nil
-    }
-  }
+	for p := 50000 + rand.Intn(1000); p < 60000; p++ {
+		l, e := net.Listen("tcp", fmt.Sprintf(":%d", p))
+		if e == nil {
+			// yey!
+			l.Close()
+			return p, nil
+		}
+	}
 
-  return 0, errors.New("error: Could not find an available port")
+	return 0, errors.New("error: Could not find an available port")
 }
 
 // WaitPort waits until you can connect to `addr`, up to `dur` amount of time
 func WaitPort(addr string, dur time.Duration) error {
-  timeout := time.Now().Add(dur)
-  for time.Now().Before(timeout) {
-    c, e := net.Dial("tcp", addr)
-    if e == nil {
-      c.Close()
-      return nil
-    }
-    time.Sleep(500 * time.Millisecond)
-  }
-  return fmt.Errorf("error: Could not connect to '%s'", addr)
+	timeout := time.Now().Add(dur)
+	for time.Now().Before(timeout) {
+		c, e := net.Dial("tcp", addr)
+		if e == nil {
+			c.Close()
+			return nil
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	return fmt.Errorf("error: Could not connect to '%s'", addr)
 }
 
 // WaitLocalPort until you can connect to `port` on localhost, up to `dur` amount of time
 func WaitLocalPort(port int, dur time.Duration) error {
-  return WaitPort(fmt.Sprintf(":%d", port), dur)
+	return WaitPort(fmt.Sprintf(":%d", port), dur)
 }
--- tcputil_test.go
+++ tcputil_test.go
@@ -1,60 +1,60 @@
 package tcputil
 
 import (
-  "fmt"
-  "log"
-  "os/exec"
-  "net"
-  "syscall"
-  "testing"
-  "time"
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+	"syscall"
+	"testing"
+	"time"
 )
 
 func Example() {
-  p, err := EmptyPort()
-  if err != nil {
-    log.Fatal(err)
-  }
+	p, err := EmptyPort()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  cmd := exec.Command("memcached", "-p", fmt.Sprintf("%d", p))
-  cmd.SysProcAttr = &syscall.SysProcAttr {
-    Setpgid: true,
-  }
-  go cmd.Run()
+	cmd := exec.Command("memcached", "-p", fmt.Sprintf("%d", p))
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
+	go cmd.Run()
 
-  err = WaitLocalPort(p, 30 * time.Second)
-  if err != nil {
-    log.Fatal(err)
-  }
+	err = WaitLocalPort(p, 30*time.Second)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  // Now do whatever with memcached!
+	// Now do whatever with memcached!
 
-  cmd.Process.Kill()
+	cmd.Process.Kill()
 }
 
 func TestBasic(t *testing.T) {
-  p, err := EmptyPort()
-  if err != nil {
-    t.Fatalf("Could not find a port to listen to: %s", err)
-  }
+	p, err := EmptyPort()
+	if err != nil {
+		t.Fatalf("Could not find a port to listen to: %s", err)
+	}
 
-  go func() {
-    t.Logf("Attempting to bind to port %d", p)
-    l, err := net.Listen("tcp", fmt.Sprintf(":%d", p))
-    if err != nil {
-      t.Fatalf("Could not listen on port %d: %s", p, err)
-    }
+	go func() {
+		t.Logf("Attempting to bind to port %d", p)
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", p))
+		if err != nil {
+			t.Fatalf("Could not listen on port %d: %s", p, err)
+		}
 
-    t.Logf("Accepting new connection...")
-    _, err = l.Accept()
-    if err != nil {
-      t.Fatalf("Could not accept connection: %s", err)
-    }
-  }()
+		t.Logf("Accepting new connection...")
+		_, err = l.Accept()
+		if err != nil {
+			t.Fatalf("Could not accept connection: %s", err)
+		}
+	}()
 
-  t.Logf("Waiting for port to be available...")
-  err = WaitLocalPort(p, 1 * time.Minute)
-  if err != nil {
-    t.Fatalf("Waited for port to open, but could not connect to %d: %s", p, err)
-  }
+	t.Logf("Waiting for port to be available...")
+	err = WaitLocalPort(p, 1*time.Minute)
+	if err != nil {
+		t.Fatalf("Waited for port to open, but could not connect to %d: %s", p, err)
+	}
 }
